b4771bbdec2e69ffa2d7ac66ab19818625e7a9a1
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-12-18 13:16:59 +0700

    Partial Prerendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 import { Metadata } from "next";
 
+export const experimental_ppr = true;
+
 export const metadata: Metadata = {
   title: "Dashboard",
 };
